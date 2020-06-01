8dc9bbc068e17b2a2871ce420ad2241927a9ab95
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-06-01 15:17:18 +0200

    Refactor URIs, endpoint names, keys and content type into String constants, and create performance tracking tests.

--- src/test/java/com/zuehlke/calculatormiddleware/SystemTest.java
+++ src/test/java/com/zuehlke/calculatormiddleware/SystemTest.java
@@ -11,14 +11,23 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Timer;
 
 import static io.restassured.RestAssured.given;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class SystemTest {
+
+    final String baseUriString = "http://localhost:";
+    final String summationEndpoint = "/cmw/summation";
+    final String multiplicationEndpoint = "/cmw/multiplication";
+    final String calculationResultKey = "result";
+    final String contentTypeJson = "application/json";
 
     @LocalServerPort
     int randomServerPort;
@@ -31,7 +40,7 @@ public class SystemTest {
 
         //When
         ResponseEntity<CalculatorResult> result = new RestTemplate().postForEntity(
-                new URI("http://localhost:" + randomServerPort + "/cmw/summation"),
+                new URI(baseUriString + randomServerPort + summationEndpoint),
                 new HttpEntity<>(listOfNumbers),
                 CalculatorResult.class);
 
@@ -44,10 +53,40 @@ public class SystemTest {
     public void shouldReturnProductOfNumbers() {
         given().
                 body(Arrays.asList(1, 3, 23)).
-                baseUri("http://localhost:" + randomServerPort).
-                contentType("application/json").
+                baseUri(baseUriString + randomServerPort).
+                contentType(contentTypeJson).
                 when().
-                post("/cmw/multiplication").
-                then().assertThat().body("result", equalTo(69));
+                post(multiplicationEndpoint).
+                then().assertThat().body(calculationResultKey, equalTo(69));
+    }
+
+    @Test
+    public void multiplicationResponseTimeShouldBeLessThan1s() {
+        given().
+                body(Arrays.asList(1, 3, 23)).
+                baseUri(baseUriString + randomServerPort).
+                contentType(contentTypeJson).
+                when().
+                post(multiplicationEndpoint).
+                then().time(lessThan(1L), SECONDS);
+    }
+
+    @Test
+    public void summationResponseTimeShouldBeLessThan1s() throws URISyntaxException {
+        //Given
+        var listOfNumbers = Arrays.asList(15, 23, 31);
+        var expectedResult = 69;
+
+        //When
+        Timer responseTimer = new Timer();
+        ResponseEntity<CalculatorResult> result = new RestTemplate().postForEntity(
+                new URI(baseUriString + randomServerPort + summationEndpoint),
+                new HttpEntity<>(listOfNumbers),
+                CalculatorResult.class);
+
+        // TODO: implement response time recording using RestTemplate API (similar to rest-assured above).
+        //Then
+        //var extractedResult = Objects.requireNonNull(result.getBody()).getResult();
+        //assertThat(extractedResult).isEqualTo(expectedResult);
     }
 }
